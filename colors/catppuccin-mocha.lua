28fd6c9d401085bd0b2a343877d541255f1c2ea1
Garbg <deee.giraffe@gmail.com>
2023-06-15 17:20:52 -0400

    Add mocha theme

---
+++ colors/catppuccin-mocha.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+style.background = { common.color "#1e1e2e" } --done
+style.background2 = { common.color "#181825" } --done
+style.background3 = { common.color "#313244" } --done
+style.text = { common.color "#cdd6f4" } --done
+style.caret = { common.color "#f5e0dc" } --done
+style.accent = { common.color "#cba6f7" } --done
+style.dim = { common.color "#bac2de" } --done
+style.divider = { common.color "#cba6f7" } --done
+style.selection = { common.color "#585b70" } --done
+style.line_number = { common.color "#6c7086" } --done
+style.line_number2 = { common.color "#cba6f7" }
+style.line_highlight = { common.color "#313244" }
+style.scrollbar = { common.color "#585b70" }
+style.scrollbar2 = { common.color "#6c7086" }
+
+style.syntax["normal"] = { common.color "#f38ba8" }
+style.syntax["symbol"] = { common.color "#b4befe" }
+style.syntax["comment"] = { common.color "#6c7086" }
+style.syntax["keyword"] = { common.color "#f38ba8" }
+style.syntax["keyword2"] = { common.color "#f38ba8" }
+style.syntax["number"] = { common.color "#fab387" }
+style.syntax["literal"] = { common.color "#b4befe" }
+style.syntax["string"] = { common.color "#a6e3a1" }
+style.syntax["operator"] = { common.color "#89dceb" }
+style.syntax["function"] = { common.color "#89b4fa" }
+
+
